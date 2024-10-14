343260e658d6f03c29e10e3c3231571a5ef15491
Sun え <42046098+iSunny0@users.noreply.github.com>
2024-10-14 16:41:18 -0300

    test
    tete

--- index.js
+++ index.js
@@ -1,58 +1,53 @@
+
 const { createInterface } = require('node:readline');
 const { execSync } = require('child_process');
 const fetch = require('node-fetch');
-const { Client, Routes } = require('discord.js');
+const { Client, Routes, REST } = require('discord.js');
+const { token } = require('./config.json'); // Mova o token para um arquivo separado por questões de segurança
 
 const ping = {
   name: 'ping',
   description: 'Pings the bot and shows the latency'
 };
 
-
 // Command Example
 const command2 = {
   name:'command2',
   description:'yes'
-}
+};
 
-const commands = [ping, command2]; // Add your commands with commas to add them to the bot!
+// Certifique-se de incluir o comando ping para evitar o erro de remoção de Entry Point Command
+const commands = [ping, command2]; // Adicione todos os comandos que o bot deve manter
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands('1041150882374483979'), {
+      body: commands,
+    });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
 // Join the Discord for support: https://discord.gg/M5MSE9CvNM
 
 const client = new Client({ intents: [] });
 const rl = createInterface({ input: process.stdin, output: process.stdout });
 
-
 client.on('interactionCreate', (interaction) => {
   if (interaction.commandName === 'ping') {
-    interaction.reply(`Latency is ${Date.now() - interaction.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);  
-  } else if(interaction.commandName === 'command2') { // This is the example command's name!
+    interaction.reply(`Latency is ${Date.now() - interaction.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+  } else if(interaction.commandName === 'command2') {
     interaction.reply('example command');
-  } else { // a response if you forget to add the command here
-    interaction.reply('this command\'s response has not been added yet!');
+  } else {
+    interaction.reply('this command's response has not been added yet!');
   }
 });
 
-const question = (q) => new Promise((resolve) => rl.question(q, resolve));
-(async ()=>{
-  const token = await question('Application token? ');
-  if(!token) throw new Error('Invalid token');
-
-  const ratelimitTest = await fetch(`https://discord.com/api/v9/invites/discord-developers`);
-
-  if(!ratelimitTest.ok) {
-    await question(`Uh oh, looks like the node you're on is currently being blocked by Discord. Press the "Enter" button on your keyboard to be reassigned to a new node. (you'll need to rerun the program once you reconnect)`)
-
-    // This kills the container manager on the repl forcing Replit to assign the repl to another node with another IP address (if the ip is globally rate limited)
-    //^ in short: Restarts the bot to be used again/attempted to start up again!
-    execSync('kill 1');
-    return;
-  };
-  
-  await client.login(token).catch((err) => {
-    throw err
-  });
-
-  await client.rest.put(Routes.applicationCommands(client.user.id), { body: commands });
-
-  console.log('DONE | Application/Bot is up and running. DO NOT CLOSE THIS TAB UNLESS YOU ARE FINISHED USING THE BOT, IT WILL PUT THE BOT OFFLINE.');
-})();
+client.login(token);
